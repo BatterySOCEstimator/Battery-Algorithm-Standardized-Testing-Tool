18cbc02212dc020383199f763a143e0c523f88ed
Aidan McLean <aidanmclean7030@yahoo.com>
2026-02-01 15:42:16 -0500

    Added baseline API structure

---
+++ battery-estimating-tool/backend/src/routes/model.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express"; 
+
+const router = Router();
+
+// router.post("/upload", upload)
+// router.get("/retrieve", retrieve)
+// router.post("/modify", modify)
+// router.post("/delete", delete)
+
+export default router;
