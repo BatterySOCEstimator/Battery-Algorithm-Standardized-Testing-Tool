b07a725c95d381b3eb17dd4463032ac5f765cf91
Aidan McLean <aidanmclean7030@yahoo.com>
2026-03-17 12:53:54 -0400

    Added Model upload and deletion endpoints

---
+++ battery-estimating-tool/backend/src/db/drizzle/migrations/0009_gifted_the_anarchist.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "models" ADD COLUMN "file_path" text NOT NULL DEFAULT '';
+ALTER TABLE "models" ADD COLUMN "status" text NOT NULL DEFAULT 'pending';
--- battery-estimating-tool/backend/src/routes/model.routes.ts
+++ battery-estimating-tool/backend/src/routes/model.routes.ts
@@ -1,9 +1,23 @@
-import { Router } from "express"; 
+import { deleteModel, uploadModel } from "@/controllers/model.controller";
+import { checkModelNameUnique, uploadMiddleware } from "@/middleware/model.middleware";
+import { requireAuth, checkBanStatus } from "@/middleware/auth.middleware";
+
+import { Router } from "express";
 const router = Router();
 
-// router.post("/upload", upload)
+
+router.post('/upload',
+    // requireAuth,         // Require authentication
+    // checkBanStatus,      // Check if user is banned
+    checkModelNameUnique,   // Check if name is unique 
+    uploadMiddleware,       // Check if valid file types 
+    uploadModel);   
+
 // router.get("/retrieve", retrieve)
 // router.post("/modify", modify)
-// router.post("/delete", delete)
+router.delete("/delete/:id", 
+    // requireAuth,         // Require authentication
+    // checkBanStatus,      // Check if user is banned
+    deleteModel)
 
 export default router;
